7ca3f79519962645717d83b085c8754443ee3fee
Josh Frank <eurosewusa@gmail.com>
2020-12-14 11:15:04 -0500

    miso soup

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+    attr_accessor :knowledge
+
+    def initialize( this_knowledge = [] )
+        @knowledge = this_knowledge
+    end
+
+    def learn( knowledge )
+        self.knowledge << knowledge
+    end
+
+    def print_knowledge
+        knowledge.each{ | fact | puts fact }
+    end
 
 end
